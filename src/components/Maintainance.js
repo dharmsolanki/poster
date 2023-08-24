d3ad904feaf53977d798b6e597e1ff3e0835a163
dharmsolanki <dharmsolanki1010@gmail.com>
2023-08-24 17:47:05 +0530

    add maintenance page

---
+++ src/components/Maintainance.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function Maintenance() {
+  return (
+    <div className="maintenance-container">
+      <img className="maintenance-image" src="/maintenance.png" alt="Maintenance" width="300" height="200" />
+      <p className="message">Sorry, the website is currently under maintenance. We'll be back soon!</p>
+      <style jsx>{`
+        .maintenance-container {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+        }
+
+        .message {
+          margin-top: 20px;
+          font-size: 18px;
+          color: #333;
+        }
+      `}</style>
+    </div>
+  );
+}
